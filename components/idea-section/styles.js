9ae2e6c105aff51945cb0b28a66c0ddd7c132495
xKarol <karolpiskorz8@gmail.com>
2022-02-19 13:26:12 +0100

    Added responsive idea section

--- components/idea-section/styles.js
+++ components/idea-section/styles.js
@@ -1,17 +1,33 @@
 import styled from "styled-components";
+import { device } from "../../styles/breakpoints";
 
 const StyledIdeaSection = styled.section`
   margin: 30px 0;
-  line-height: 1.4;
+  line-height: 1.2;
   margin-bottom: 50px;
 `;
 const StyledIdeaHeading = styled.h2`
   margin-bottom: 30px;
-  font-size: 30px;
+  font-size: 32px;
   font-family: ${(props) => props.theme.fonts.mainBold};
+  letter-spacing: -2px;
+  @media ${device.md} {
+    font-size: 45px;
+  }
+  @media ${device.lg} {
+    margin-bottom: 60px;
+    font-size: 4.5vw;
+  }
 `;
 const StyledIdeaText = styled.p`
   font-size: 22px;
+  line-height: 1.6;
+  @media ${device.md} {
+    font-size: 24px;
+  }
+  @media ${device.lg} {
+    font-size: 2.5vw;
+  }
 `;
 
 export { StyledIdeaSection, StyledIdeaHeading, StyledIdeaText };
