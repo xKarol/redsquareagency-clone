0479644d116c3246165c50747bbf730c46cfb340
xKarol <karolpiskorz8@gmail.com>
2022-03-05 11:38:03 +0100

    Added more transitions on work page

--- components/pages/work.js
+++ components/pages/work.js
@@ -3,27 +3,38 @@ import Footer from "../footer";
 import Header from "../header";
 import { ImagesList, StyledImage } from "../preview-images/styles";
 import { motion } from "framer-motion";
+import { useContext } from "react";
+import AppContext from "../../context/app-context";
 
 function Work({ data }) {
   const { images } = data;
+  const { firstLoad } = useContext(AppContext);
+
   return (
     <>
       <Header />
       <motion.div exit={{ opacity: 0, transition: { duration: 0.8 } }}>
         <ImagesList>
           {images.map((image) => (
-            <StyledImage
+            <motion.div
               key={image.id}
-              className={`active list scale-brightness-transition`}
+              variants={{ visible: { scale: 1.05 }, firstLoad: { opacity: 0 } }}
+              initial={firstLoad ? "firstLoad" : "visible"}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.4, type: "spring", bounce: 0.5 }}
             >
-              <Image
-                src={image.path}
-                alt={image.alt}
-                layout="fill"
-                objectFit="cover"
-                priority
-              />
-            </StyledImage>
+              <StyledImage
+                className={`active list scale-brightness-transition`}
+              >
+                <Image
+                  src={image.path}
+                  alt={image.alt}
+                  layout="fill"
+                  objectFit="cover"
+                  priority
+                />
+              </StyledImage>
+            </motion.div>
           ))}
         </ImagesList>
       </motion.div>
