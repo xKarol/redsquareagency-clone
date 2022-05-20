8914216a1e073aef7d0c2dca5431157940f7d6bd
xKarol <karolpiskorz8@gmail.com>
2022-05-20 13:17:49 +0200

    Improved header text change animation

--- components/header/HeaderLink.js
+++ components/header/HeaderLink.js
@@ -2,13 +2,15 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { motion, useAnimation } from "framer-motion";
 import AppContext from "../../context/app-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import HeaderNav from "./HeaderNav";
 
 function HeaderLink() {
   const { pathname } = useRouter();
   const controls = useAnimation();
   const { firstLoad } = useContext(AppContext);
+  const headerText = pathname === "/" ? "Red Square" : "Work";
+  const [headerName, setHeaderName] = useState(headerText);
 
   const variants = {
     visible: {
@@ -25,10 +27,11 @@ function HeaderLink() {
       initial="hidden"
       variants={variants}
       exit={{ translateX: 25, opacity: 0, transition: { duration: 0.4 } }}
+      onAnimationComplete={() => setHeaderName(headerText)}
     >
       <Link href={pathname === "/" ? "/work" : "/"}>
         <a>
-          <HeaderNav>{pathname === "/" ? "Work" : "Red Square"}</HeaderNav>
+          <HeaderNav>{headerName}</HeaderNav>
         </a>
       </Link>
     </motion.div>
